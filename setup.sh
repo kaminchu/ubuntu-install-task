6ba401731bb6dcf5570b12233022c2287f36529c
kaminchu <kmchu.mail@gmail.com>
2018-02-24 12:04:46 +0900

    bashに読み込まれないからむりやり

--- setup.sh
+++ setup.sh
@@ -18,7 +18,6 @@ sudo apt install -y git curl xsel
 touch ~/.bash_aliases
 echo 'export PS1="\u@\h:\W\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
 echo "alias pbcopy='xsel --clipboard --input" >> ~/.bash_aliases
-source ~/.bashrc
 
 ## git
 git config --global core.excludesfile ~/.gitignore_global
@@ -31,7 +30,9 @@ echo '.Trash-*' >> /.gitignore_global
 git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(anyenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
+
 
 ## goenv
 anyenv install goenv
@@ -39,14 +40,14 @@ goenv install 1.9.5
 goenv global 1.9.5
 echo 'export GOPATH=$HOME/.go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
-source ~/.bashrc
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin
 
 ## ghq peco
 go get github.com/peco/peco/cmd/peco
 go get github.com/motemen/ghq
 git config --global ghq.root ~/.ghq/src
 echo "alias cr='cd $(ghq list -p | peco)'" >> ~/.bash_aliases
-source ~/.bashrc
 
 ## vscode
 sudo snap install -y --classic vscode
