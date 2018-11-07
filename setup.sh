ef0d8f19ba1b0a1500fa81ead1e915ee338bdf35
kaminchu <kmchu.mail@gmail.com>
2018-11-07 10:10:55 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,10 @@ echo '*~' >> ~/.gitignore_global
 echo './idea' >> ~/.gitignore_global
 echo '.Trash-*' >> ~/.gitignore_global
 
+## fzf
+git clone https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
 ## anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc
@@ -49,28 +53,34 @@ eval "$(anyenv init -)"
 sudo snap install go --classic
 echo 'export GOPATH=$HOME/.go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+source ~/.bashrc
 
-# この後のgo getで使いたいから
-export GOPATH=$HOME/.go
-export PATH=$PATH:$GOPATH/bin
-
-## ghq peco
-go get github.com/peco/peco/cmd/peco
+## ghq ~~peco~~ fzf
+# go get github.com/peco/peco/cmd/peco
 go get github.com/motemen/ghq
 git config --global ghq.root ~/.ghq/src
-echo "alias cr='cd \$(ghq list -p | peco)'" >> ~/.bash_aliases
+echo "alias cr='cd \$(ghq list -p | fzf --reverse)'" >> ~/.bash_aliases
 
 ## vscode
 # Ubuntu18.10ではsnapでvscode入れると激遅プンプン丸だったので、素直にdeb落として入れたほうがいい
 # sudo snap install --classic vscode
 
-## idea
+## idea(あとでアクティベートするように)
 sudo snap install intellij-idea-ultimate --classic
 echo "alias idea='intellij-idea-ultimate $1'" >> ~/.bash_aliases
 source ~/.bashrc
 
-## gitkraken
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb -P ~/Downloads
-dpkg -i ~/Downloads/gitkraken-amd64.deb
-echo "alias gk='gitkraken -p $1'" >> ~/.bash_aliases
-source ~/.bashrc
+## ~~gitkraken~~ 商用利用ダメっぽいので入れない
+# wget https://release.gitkraken.com/linux/gitkraken-amd64.deb -P ~/Downloads
+# dpkg -i ~/Downloads/gitkraken-amd64.deb
+# echo "alias gk='gitkraken -p $1'" >> ~/.bash_aliases
+# source ~/.bashrc
+
+## 手動でやること
+# - Chromeのdebをダウンロードして実行
+# - VSCodeのdebをダウンロードして実行
+# - VSCodeの日本語化
+# - ideaのアクティベート
+# - アイコンをいい感じにする
+# - ディスプレイの配置調整(4KとfullHDを混在させると、サイズ調整がうまくいかないのなんとかならないかな)
+# - 秘密鍵の作成`ssh-keygen -t rsa`と公開鍵`cat ~/.ssh/id_rsa.pub`の各種サービスへの登録
